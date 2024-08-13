0478b4b65aa0eeefe9783cfcc9cea684deb59804
Shane <6071159+smashedr@users.noreply.github.com>
2024-08-13 13:27:42 -0700

    Add CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`listening on port: ${port}`)
